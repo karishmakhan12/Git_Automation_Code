59c5104506977f14cb33c263d93675ee211a672b
Karishma Khan <karishmakhanbangalore@outlook.com>
2023-11-28 17:52:24 +0530

    initial commit with git repo created

---
+++ src/com/git/CommonConstant.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class CommonConstant {
+
+	public static final String WEBSITE_ADDRESS = "https://www.github.com/";
+	public static final String EMAIL_ADDRESS = "karishmakhanbangalore@outlook.com";
+	public static final String PASSWORD_ADDRESS = "Testing@1221";
+	public static final String SINGIN_BUTTON_PATH = "/html/body/div[1]/div[1]/header/div/div[2]/div/div/div/a";
+	public static final String EMAIL_PATH = "//*[@id=\"login_field\"]";
+	public static final String PASSWORD_PATH = "//*[@id=\"password\"]";
+	public static final String SINGIN_PATH = "//*[@id=\"login\"]/div[4]/form/div/input[13]";
+	public static final String NEW_BTN_PATH = "/html/body/div[1]/div[6]/div/div/aside/div/div/loading-context/div/div[1]/div/h2/a/span";
+	public static final String REP_NAME_PATH = "//*[@id=\":r2:\"]";
+	public static final String REP_NAME = "Git_Automation_Code2";
+	public static final String CREAT_PATH = "/html/body/div[1]/div[6]/main/react-app/div/form/div[5]/button/span/span";
+
+}
---
+++ src/com/git/GithubAutomation.java
@@ -0,0 +1,29 @@
+package com.git;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GithubAutomation {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get(CommonConstant.WEBSITE_ADDRESS);
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		Thread.sleep(1000);
+		driver.findElement(By.xpath(CommonConstant.SINGIN_BUTTON_PATH)).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(CommonConstant.EMAIL_PATH)).sendKeys(CommonConstant.EMAIL_ADDRESS);
+		driver.findElement(By.xpath(CommonConstant.PASSWORD_PATH)).sendKeys(CommonConstant.PASSWORD_ADDRESS);
+		driver.findElement(By.xpath(CommonConstant.SINGIN_PATH)).click();
+		driver.getCurrentUrl();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(CommonConstant.NEW_BTN_PATH)).click();
+		driver.findElement(By.xpath(CommonConstant.REP_NAME_PATH)).sendKeys(CommonConstant.REP_NAME);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,350)", "");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(CommonConstant.CREAT_PATH)).click();
+	}
+}
